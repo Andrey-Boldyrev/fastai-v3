02c1196736806230f3e9a7abca097d252f2959e7
Andrey_Boldyrev <31937438+Andrey-Boldyrev@users.noreply.github.com>
2019-12-05 03:32:51 +0300

    Update server.py
    change classes and URL

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1ux5VioC81ABPp4OGrm2M3WFffNEb0zX1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['lakhta_center', 'burj_khalifa', 'empire_state_building']
 path = Path(__file__).parent
 
 app = Starlette()
